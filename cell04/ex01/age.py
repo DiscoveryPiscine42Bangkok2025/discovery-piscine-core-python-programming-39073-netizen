e1ba4858c3985f913ab8d40592a4ab01f5ac1048
39073-netizen <39073@rwb.ac.th>
2026-04-27 03:42:15 +0000

    01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+i = int(input("Please tell me your age: "))
+
+print(f"You are currently {i} years old.")
+
+a = 10
+while a <= 30:
+    print(f"In {a} years, you'll be {i + a} years old.")
+    a += 10
